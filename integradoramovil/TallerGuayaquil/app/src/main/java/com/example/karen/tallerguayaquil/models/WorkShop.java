7e71d6eacbb82945f5371c12beb32502b89e2dfd
Karen Ponce <gatita89_pretty@hotmail.com>
2017-07-27 06:03:59 -0500

    [ADD] workshop model to models package

---
+++ integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/models/WorkShop.java
@@ -0,0 +1,131 @@
+package com.example.karen.tallerguayaquil.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by karen on 26/07/17.
+ */
+
+public class WorkShop implements Serializable {
+
+    private float latitude;
+    private float longitude;
+
+    private String username;
+    private String password;
+    private String address;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String managerName;
+    private String workshopName;
+
+    private List<Brand> brandList;
+    private List<Service> serviceList;
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+
+    public String getWorkshopName() {
+        return workshopName;
+    }
+
+    public void setWorkshopName(String workshopName) {
+        this.workshopName = workshopName;
+    }
+
+    public List<Brand> getBrandList() {
+        return brandList;
+    }
+
+    public void setBrandList(List<Brand> brandList) {
+        this.brandList = brandList;
+    }
+
+    public List<Service> getServiceList() {
+        return serviceList;
+    }
+
+    public void setServiceList(List<Service> serviceList) {
+        this.serviceList = serviceList;
+    }
+}
