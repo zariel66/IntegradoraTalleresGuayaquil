02471c4d4a25ffef3566e08cbba801d0a99aaf76
Karen Ponce <gatita89_pretty@hotmail.com>
2017-07-31 09:57:51 -0500

    [ADD] Person model (models)

---
+++ integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/models/Person.java
@@ -0,0 +1,109 @@
+package com.example.karen.tallerguayaquil.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+
+public class Person implements Serializable {
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("username")
+    private String username;
+
+    @SerializedName("password")
+    private String password;
+
+    @SerializedName("password_confirmation")
+    private String passwordConfirmation;
+
+    @SerializedName("correo")
+    private String email;
+
+    @SerializedName("nombre")
+    private String firstName;
+
+    @SerializedName("apellido")
+    private String lastName;
+
+    @SerializedName("modelo")
+    private String modelName;
+
+    @SerializedName("marca")
+    private Brand brand;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        this.passwordConfirmation = password;
+    }
+
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+}
