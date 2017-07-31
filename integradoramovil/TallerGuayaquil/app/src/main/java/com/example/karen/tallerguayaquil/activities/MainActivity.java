fa8c29fb4956e3f196be24038ccdda2aee9dd2c6
Karen Ponce <gatita89_pretty@hotmail.com>
2017-07-31 10:07:09 -0500

    [FIX] MainActivity

--- integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/activities/MainActivity.java
+++ integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/activities/MainActivity.java
@@ -1,22 +1,62 @@
 package com.example.karen.tallerguayaquil.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.example.karen.tallerguayaquil.R;
+import com.example.karen.tallerguayaquil.adapters.CategoryAdapter;
+import com.example.karen.tallerguayaquil.listeners.RecyclerTouchListener;
+import com.example.karen.tallerguayaquil.models.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private GridLayoutManager mGridLayoutManager;
+    private CategoryAdapter mCategoryAdapter;
+    private List<Category> mCategoryList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void mecanicos(View view) {
-        Intent i = new Intent(this, MapActivity.class);
-        startActivity(i);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_categories);
+
+        mGridLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+
+        populate(mCategoryList);
+        mCategoryAdapter = new CategoryAdapter(MainActivity.this, mCategoryList);
+        mRecyclerView.setAdapter(mCategoryAdapter);
+
+        // Set handler clicked
+        mRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(
+                this, mRecyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Intent i = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+    }
+    private void populate(List<Category> mCategoryList) {
+
+        mCategoryList.add(new Category(1,"Mecánica", R.drawable.tallerm));
+        mCategoryList.add(new Category(1,"Electrico", R.drawable.bateria));
+        mCategoryList.add(new Category(1,"Carrocería", R.drawable.lata));
+        mCategoryList.add(new Category(1,"Tapizado", R.drawable.silla));
     }
 }
