8864d0cdaf1cb4ad0f7618e7b8c0767a2e28c8ef
Karen Ponce <gatita89_pretty@hotmail.com>
2017-07-31 10:03:16 -0500

    [FIX] SignupPersonActivity and SignupVehicleActivity (activities)

--- integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/activities/SignupPersonActivity.java
+++ integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/activities/SignupPersonActivity.java
@@ -1,64 +1,126 @@
 package com.example.karen.tallerguayaquil.activities;
 
 import android.content.Intent;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.example.karen.tallerguayaquil.R;
+import com.example.karen.tallerguayaquil.models.Person;
+import com.example.karen.tallerguayaquil.utils.Util;
 
 
-public class SignupPersonActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignupPersonActivity extends AppCompatActivity {
 
-    //Definiendo Views
-     EditText txtnombre, txtapellido, txtcorreo, txtusuario, txtpassword;
-    private Button btnsiguiente;
-    //Variable tipo cliente
-
+    private EditText mFirstNameView, mLastNameView,
+            mEmailView, mUsernameView, mPasswordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_person);
 
-        //Inicializando Views
-        txtnombre = (EditText) findViewById(R.id.txtnombre);
-        txtapellido = (EditText) findViewById(R.id.txtapellido);
-        txtcorreo = (EditText) findViewById(R.id.txtcorreo);
-        txtusuario = (EditText) findViewById(R.id.txtusuario);
-        txtpassword = (EditText) findViewById(R.id.txtpassword);
-        btnsiguiente = (Button) findViewById(R.id.btn_siguiente);
-        btnsiguiente.setOnClickListener(this);
+        mFirstNameView = (EditText) findViewById(R.id.txt_first_name);
+        mLastNameView = (EditText) findViewById(R.id.txt_last_name);
+        mEmailView = (EditText) findViewById(R.id.txt_email);
+        mUsernameView = (EditText) findViewById(R.id.txt_username);
+        mPasswordView = (EditText) findViewById(R.id.txt_password);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE) {
+                    Util.hideSoftKeyboard(getApplicationContext(), getCurrentFocus());
+                    attemptNext(null);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-       if (txtnombre.getText().toString().equals("") || txtapellido.getText().toString().equals("") || txtcorreo.getText().toString().equals("") || txtusuario.getText().toString().equals("") || txtpassword.getText().toString().equals("")) {
-            Toast.makeText(SignupPersonActivity.this, "Por favor, llene todos los campos", Toast.LENGTH_LONG).show();
-           } else {
-                  String nombre=txtnombre.getText().toString();
-                  String apellido=txtapellido.getText().toString();
-                  String correo=txtcorreo.getText().toString();
-                  String usuario=txtusuario.getText().toString();
-                  String password=txtpassword.getText().toString();
+    public void attemptNext(View v) {
 
-                  Bundle parametros = new Bundle();
-                  parametros.putString("nombre", nombre);
-                  parametros.putString("apellido", apellido);
-                  parametros.putString("correo", correo);
-                  parametros.putString("usuario", usuario);
-                  parametros.putString("password", password);
+        // Init progress message
+        Util.showLoading(SignupPersonActivity.this, getString(R.string.title_progress_validation_message));
+
+        // Reset errors.
+        mFirstNameView.setError(null);
+        mLastNameView.setError(null);
+        mEmailView.setError(null);
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        String firsName = mFirstNameView.getText().toString();
+        String lastName = mLastNameView.getText().toString();
+        String email = mEmailView.getText().toString().trim();
+        String username = mUsernameView.getText().toString().trim();
+        String password = mPasswordView.getText().toString();
 
 
-                Intent i = new Intent(this, SignupVehicleActivity.class);
+        boolean cancel = false;
+        View focusView = null;
 
-                i.putExtras(parametros);
-                startActivity(i);
-       }
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!Util.isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(lastName)) {
+            mLastNameView.setError(getString(R.string.error_incorrect_text_field));
+            focusView = mLastNameView;
+            cancel = true;
+        }
+
+        if (!Util.isNameValid(firsName)) {
+            mFirstNameView.setError(getString(R.string.error_incorrect_text_field));
+            focusView = mFirstNameView;
+            cancel = true;
+        }
+
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+
+            Person person = new Person();
+            person.setFirstName(firsName);
+            person.setLastName(lastName);
+            person.setEmail(email);
+            person.setUsername(username);
+            person.setPassword(password);
+
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("person", person);
+
+            Intent i = new Intent(SignupPersonActivity.this, SignupVehicleActivity.class);
+            i.putExtras(bundle);
+            startActivity(i);
+        }
+
+        Util.hideLoading();
     }
 }
 
--- integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/activities/SignupVehicleActivity.java
+++ integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/activities/SignupVehicleActivity.java
@@ -1,107 +1,196 @@
 package com.example.karen.tallerguayaquil.activities;
 
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.karen.tallerguayaquil.R;
+import com.example.karen.tallerguayaquil.models.Brand;
+import com.example.karen.tallerguayaquil.models.Person;
+import com.example.karen.tallerguayaquil.models.WorkShop;
+import com.example.karen.tallerguayaquil.utils.ApiService;
+import com.example.karen.tallerguayaquil.utils.ServiceGenerator;
+import com.example.karen.tallerguayaquil.utils.Util;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 
-public class SignupVehicleActivity extends AppCompatActivity implements Spinner.OnItemSelectedListener{
+public class SignupVehicleActivity extends AppCompatActivity{
 
     //Declaring an Spinner
-      private Spinner spinner;
-    //An ArrayList for Spinner Items
-      ArrayList<String> marcas;
-     EditText txtmodelo;
-     Button btnregistrar;
+    private ArrayAdapter<Brand> brandArrayAdapter;
+    private Spinner mVehicleBrands;
+    private Person person;
+
+    private EditText mModelView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_vehicle);
 
+        // Get workshop
+        person = (Person) getIntent().getExtras().getSerializable("person");
 
-        //Initializing the ArrayList
-        marcas = new ArrayList<String>();
-        spinner = (Spinner) findViewById(R.id.spin_marcas);
-        txtmodelo = (EditText) findViewById(R.id.txtmodelo);
-        btnregistrar = (Button) findViewById(R.id.btnregistrar);
+        mVehicleBrands = (Spinner) findViewById(R.id.spn_vehicle_brand);
+        mModelView = (EditText) findViewById(R.id.txt_model);
 
-       // btnregistrar.setOnClickListener(this);
-
-        spinner.setOnItemSelectedListener(this);
-         getData();
+        brandArrayAdapter = new ArrayAdapter<Brand>(this, android.R.layout.select_dialog_singlechoice);
+        mVehicleBrands.setAdapter(brandArrayAdapter);
     }
 
 
-    private void getData(){
-        String url ="http://192.168.0.4:8000/api/marcas";
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-                        try {
+        if (Util.isNetworkAvailable(getApplicationContext())) {
+            Util.showLoading(SignupVehicleActivity.this, "Cargando...");
+            brandsTask();
+        } else {
+            Util.showToast(getApplicationContext(), getString(R.string.message_network_connectivity_failed));
+        }
+    }
 
-                            JSONArray jsonObj = new JSONArray(response);
+    public void attemptSignup(View v) {
 
-                            JSONObject obj = new JSONObject();
-                            for (int i = 0; i < jsonObj.length(); i++) {
-                                JSONObject c = jsonObj.getJSONObject(i);
-                                String result = c.getString("nombre");
-                                marcas.add(result);
-                            }
-                            spinner.setAdapter(new ArrayAdapter<String>(SignupVehicleActivity.this, android.R.layout.simple_spinner_dropdown_item, marcas));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+
+        // Reset errors.
+        mModelView.setError(null);
+
+        String modelName = mModelView.getText().toString();
+        Brand brand = (Brand) mVehicleBrands.getSelectedItem();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (!Util.isNameValid(modelName)) {
+            mModelView.setError(getString(R.string.error_field_required));
+            focusView = mModelView;
+            cancel = true;
+        }
+
+        if (brand.getId() != 0){
+            Util.showToast(getApplicationContext(), "Debe seleccionar una marca");
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            if (Util.isNetworkAvailable(getApplicationContext())) {
+
+                //Util.showLoading(SignupVehicleActivity.this, "Registrando...");
+
+                person.setModelName(modelName);
+                person.setBrand(brand);
+
+                //signupTask(person);
+
+                // Init Login
+                Intent intent = new Intent(SignupVehicleActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            } else {
+                Util.showToast(
+                        getApplicationContext(), getString(R.string.message_network_connectivity_failed));
+            }
+        }
+    }
+
+    /**
+     * Brands task
+     */
+    private void brandsTask(){
+        ApiService api = ServiceGenerator.createApiService();
+
+        Call<List<Brand>> call = api.getBrands();
+        call.enqueue(new Callback<List<Brand>>() {
+
+            @Override
+            public void onResponse(Call<List<Brand>> call, retrofit2.Response<List<Brand>> response) {
+                if (response.isSuccessful()) {
+                    List<Brand> brands = response.body();
+
+                    if (!brands.isEmpty()) {
+                        for (Brand b : brands) {
+                            brandArrayAdapter.add(b);
                         }
+                        brandArrayAdapter.notifyDataSetChanged();
+
+                    } else {
+                        Util.showToast(getApplicationContext(), getString(R.string.message_service_server_empty));
                     }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+                } else {
+                    Util.showToast(getApplicationContext(), getString(R.string.message_service_server_failed));
+                }
+                Util.hideLoading();
+            }
 
-                    }
-                });
-
-          RequestQueue requestQueue = Volley.newRequestQueue(this);
-        //Adding request to the queue
-        requestQueue.add(stringRequest);
+            @Override
+            public void onFailure(Call<List<Brand>> call, Throwable t) {
+                Util.showToast(getApplicationContext(), getString(R.string.message_network_local_failed));
+                Util.hideLoading();
+            }
+        });
     }
 
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    /**
+     * Signup Task
+     */
+    private void signupTask(WorkShop workShop){
 
+        ApiService auth = ServiceGenerator.createApiService();
+
+        Call<Void> call = auth.signupPerson(person);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull retrofit2.Response<Void> response) {
+
+                if (response.isSuccessful()) {
+                    //if (!userResponse.isError()) {
+
+                    Util.showToast(getApplicationContext(), "Registrado correctamente");
+
+                    // Init Login
+                    Intent intent = new Intent(SignupVehicleActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                    /*} else {
+                        // Show message error
+                        Util.showToast(getApplicationContext(), "Revise los parametros");
+                    }*/
+                } else {
+                    Util.showToast(getApplicationContext(),
+                            getString(R.string.message_service_server_failed));
+                }
+                Util.hideLoading();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
+
+                Util.showToast(getApplicationContext(),
+                        getString(R.string.message_network_local_failed));
+                Util.hideLoading();
+            }
+        });
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
-
+    /*
 
     private void registerUser( final String nombre, final String apellido, final String username, final String password, final String correo){
        final String modelo=txtmodelo.getText().toString();
@@ -148,7 +237,7 @@ public class SignupVehicleActivity extends AppCompatActivity implements Spinner.
             registerUser(nombre,apellido,username,password,correo);
         }
 
-    }
+    }*/
 
 }
 
