1a05b34ecaf127115e335598117a854145144400
Karen Ponce <gatita89_pretty@hotmail.com>
2017-07-31 10:06:47 -0500

    [FIX] MapActivity

--- integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/activities/MapActivity.java
+++ integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/activities/MapActivity.java
@@ -1,19 +1,36 @@
 package com.example.karen.tallerguayaquil.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.karen.tallerguayaquil.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity {
-
+public class MapActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
-
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng guayaquil = new LatLng(-2.203816, -79.897453);
+        Marker marker = mMap.addMarker(
+                new MarkerOptions()
+                        .position(guayaquil)
+                        .draggable(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(guayaquil));
+
+    }
 }
