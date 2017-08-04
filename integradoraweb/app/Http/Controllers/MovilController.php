2c735dbcc84ce4e999f38ce4b6894857506ad10f
Karen Ponce <gatita89_pretty@hotmail.com>
2017-08-04 10:27:04 -0500

    [ADD] login to api

--- integradoraweb/app/Http/Controllers/MovilController.php
+++ integradoraweb/app/Http/Controllers/MovilController.php
@@ -6,119 +6,32 @@ use DB;
 use Hash;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
-use App\User;
-use App\Http\Controllers\Controller;
-use App\Marca;
-use App\Taller;
-use App\Calificacion;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use App\Marca;
+use App\Taller;
+use App\Calificacion;
+use App\Vehiculo;
+
 
 class MovilController extends Controller
 {
-    public function obtenermarcas()
+  public function obtenerMarcas()
 	{
 		return response()->json(Marca::all());
-	}
-
-
-  public function camposobligatorios()
-    {
-
-      $rules = array(
-        'nombre' => 'required|regex:/(^[A-Za-z ]+$)+/|min:3',
-        'apellido' => 'required|regex:/(^[A-Za-z ]+$)+/|min:3',
-        'username' => 'required|unique:usuario,username|min:5',
-        'correo' => 'required|unique:usuario,correo|email',
-        'password' => 'required|min:8'
-      );
-
-      $custom = array(
-        "nombre.required" => "El :attribute es requerido",
-        "apellido.required" => "El :attribute es requerido",
-        "username.required" => "El usuario es requerido",
-        "password.required" => "La contraseña es requerida",
-        "correo.required" => "El :attribute es requerido",
-
-        "username.unique" => "El usuario ingresado ya existe, elija otro",
-        "correo.unique" => "El :attribute ingresado ya se encuentra en uso, elija otro",
-
-        "correo.email" => "Ingrese una dirección de correo válida",
-
-
-        "nombre.regex" => "El :attribute solo debe contener texto",
-        "apellido.regex" => "El :attribute solo debe contener texto",
-
-        "nombre.min" => "El :attribute debe tener mínimo :min caracteres",
-        "apellido.min" => "El :attribute debe tener mínimo :min caracteres",
-        "username.min" => "El :attribute debe tener mínimo :min caracteres",
-        "password.min" => "La contraseña debe tener mínimo :min caracteres",
-      );
-
-      $validation = Validator::make(Input::all(),$rules,$custom);
-
-      if ($validation->fails())
-      {
-        return response()->back()->withErrors($validator)->withInput();
-      }
-	}
-
-
-	public function ingresarclientes(Request $request)
-	{
-		/*$rules = array(
-
-			"marca" => 'required',
-			'modelo' => "required",
-			);
-
-
-		$custom = array(
-
-     		"marca.required" => "La :attribute es requerida",
-			"modelo.required" => "La :attribute del vehículo es requerido",
-
-			);
-
-		$validation = Validator::make(Input::all(),$rules,$custom);
-
-		if ($validation->fails())
-		{
-			error_log("entro controller 3");
-			return Redirect::back()->withErrors($validation)->withInput(Input::all());
-		}*/
-
-		DB::beginTransaction();
-		try {
-			$idusuario = DB::table('usuario')->insertGetId(
-				array(
-
-					'nombre' => Input::get('nombre'),
-					'apellido' => Input::get('apellido'),
-					'tipo' => 2,
-					'username' => Input::get('username'),
-					'password' => bcrypt(Input::get('password')),
-					'correo' => Input::get('correo')
-
-					)
-				);
-			$idvehiculo = DB::table('vehiculo')->insertGetId(
-				array(
-
-					'idmarca' => Input::get('marca'),
-					'idusuario' => $idusuario,
-					'modelo' => Input::get('modelo')
-					)
-				);
-			error_log($idusuario);
-			error_log($idvehiculo);
-		} catch (\Exception $e) {
-			DB::rollback();
-			throw $e;
-		}
   }
+
+  public function obtenerVehiculos()
+  {
+    //$user = $return->user();
+
+		return response()->json(Vehicule::all());
+	}
 
   public function registrarCliente()
   {
@@ -349,4 +262,38 @@ class MovilController extends Controller
       'msg' => 'Taller registrado correctamente'
     ]);
   }
+
+  public function iniciarSesion()
+  {
+
+    $username = Input::get('username');
+    $password = Input::get('password');
+
+    try {
+      if (Auth::attempt(['username' => $username, 'password' => $password]))
+      {
+        $user = Auth::user();
+        return response()->json([
+          'is_error' => false,
+          'msg' => 'Inicio de sesión exitoso',
+          'data' => $user
+        ]);
+      }
+      return response()->json([
+        'is_error' => false,
+        'msg' => 'Usuario o contraseña inválido',
+        'username' => $username,
+        'password' => $password
+      ]);
+
+    } catch (\Exception $e) {
+      return response()->json([
+        'error' => $e->getMessage(),
+        'username' => $username,
+        'password' => $password
+      ]);
+      //abort(500);
+    }
+  }
+
 }
--- integradoraweb/routes/api.php
+++ integradoraweb/routes/api.php
@@ -18,5 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/marcas', 'MovilController@obtenermarcas');
-Route::post('/clientes', 'MovilController@ingresarclientes');
+Route::get('/marcas', 'MovilController@obtenerMarcas');
+Route::get('/vehiculos', 'MovilController@obtenerVehiculos');
+
+
+Route::post('/registroclientesubmit', 'MovilController@registrarCliente');
+Route::post('/registrotallersubmit', 'MovilController@registrarTaller');
+Route::post('/iniciarsesion', 'MovilController@iniciarSesion');
