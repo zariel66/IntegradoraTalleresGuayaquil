575a34d88c707691b5ad6d08dfce0ef501e87575
Karen Ponce <gatita89_pretty@hotmail.com>
2017-08-03 19:06:44 -0500

    [ADD] registrar taller in MovilController (controllers)

--- integradoraweb/app/Http/Controllers/MovilController.php
+++ integradoraweb/app/Http/Controllers/MovilController.php
@@ -220,4 +220,133 @@ class MovilController extends Controller
       'data' => $user
     ]);
   }
+
+  public function registrarTaller()
+  {
+    $rules = array(
+      'nombre' => 'required|regex:/(^[A-Za-záéíóú ]+$)+/|min:3',
+      'apellido' => 'required|regex:/(^[A-Za-záéíóú ]+$)+/|min:3',
+      'username' => 'required|unique:usuario,username|min:5',
+      'correo' => 'required|unique:usuario,correo|email',
+      'password' => 'required|min:8',
+      'password_confirmation' => 'same:password',
+      'direccion' => 'required|min:10',
+      'telefono' => 'required|min:6',
+      'nombre_empleado' => 'required|min:10',
+      'marcas' => 'required',
+      'servicios' => 'required',
+      'nombre_taller' => "required|min:3",
+    );
+
+
+
+    $custom = array(
+      "nombre.required" => "El :attribute es requerido",
+      "apellido.required" => "El :attribute es requerido",
+      "username.required" => "El usuario es requerido",
+      "password.required" => "La contraseña es requerida",
+      "correo.required" => "El :attribute es requerido",
+
+      "telefono.required" => "El teléfono es requerido",
+      "direccion.required" => "La dirección es requerida",
+      "nombre_empleado.required" => "El nombre del empleado es requerido",
+      "nombre_taller.required" => "El nombre del taller es requerido",
+      "marcas.required" => "Debe seleccionar al menos una marca de vehículo con la que trabaja",
+      "servicios.required" => "Debe seleccionar al menos un servicio que ofrece en su taller",
+
+      "username.unique" => "El usuario ingresado ya existe, elija otro",
+      "correo.unique" => "El :attribute ingresado ya se encuentra en uso, elija otro",
+
+      "correo.email" => "Ingrese una dirección de correo válida",
+
+      "password.confirmed" => "La contraseña debe ser confirmada",
+      "password_confirmation.same" => "Las contraseñas deben coincidir",
+
+      "nombre.regex" => "El :attribute solo debe contener texto",
+      "apellido.regex" => "El :attribute solo debe contener texto",
+      "username.regex" => "El nombre de usuario debe comenzar con una letra seguido de caracteres alfanuméricos",
+
+      "nombre.min" => "El :attribute debe tener mínimo :min caracteres",
+      "apellido.min" => "El :attribute debe tener mínimo :min caracteres",
+      "username.min" => "El usuario debe tener mínimo :min caracteres",
+      "password.min" => "La contraseña debe tener mínimo :min caracteres",
+
+      "telefono.min" => "El teléfono debe tener mínimo :min caracteres",
+      "direccion.min" => "La dirección debe tener mínimo :min caracteres",
+      "nombre_empleado.min" => "El nombre del empleado debe tener mínimo :min caracteres",
+      "nombre_taller.min" => "El nombre del taller debe tener mínimo :min caracteres",
+    );
+
+    $validation = Validator::make(Input::all(),$rules,$custom);
+
+    if ($validation->fails())
+    {
+      return response()->json([
+        'is_error' => true,
+        'msg' => $validation->errors()->first()
+      ]);
+    }
+
+    $api_token = str_random(60);
+    $input = array('api_token' => $api_token );
+
+    DB::beginTransaction();
+    try {
+
+      $idusuario = DB::table('usuario')->insertGetId(
+        array(
+          'nombre' => Input::get('nombre'),
+          'apellido' => Input::get('apellido'),
+          'correo' => Input::get('correo'),
+          'password' => bcrypt(Input::get('password')),
+          'tipo' => 1,
+          'username' => Input::get('username'),
+          'api_token' => $api_token
+        )
+      );
+
+      $idtaller = DB::table('taller')->insertGetId(
+        array(
+          'latitud' => Input::get('lat'),
+          'longitud' => Input::get('lon'),
+          'direccion' => Input::get('direccion'),
+          'idusuario' => $idusuario,
+          'nombre_empleado' => Input::get('nombre_empleado'),
+          'telefono' => Input::get('telefono'),
+          'nombre_taller' => Input::get('nombre_taller'),
+        )
+      );
+
+      foreach (Input::get('marcas') as $marca)
+      {
+        $id_marca_taller = DB::table('marca_taller')->insertGetId(
+          array(
+            'idmarca' => $marca['id'],
+            'idtaller' => $idtaller
+          )
+        );
+      }
+
+      foreach (Input::get('servicios') as $servicio)
+      {
+        $id_servicio_taller = DB::table('servicio_taller')->insertGetId(
+          array(
+            'categoria' => $servicio['nombre'],
+            'idtaller' => $idtaller
+          )
+        );
+      }
+
+    } catch (\Exception $e) {
+      DB::rollback();
+      return response()->json(['error' => $e->getMessage()]);
+      //abort(500);
+    }
+
+    DB::commit();
+    return response()->json([
+      'is_error' => false,
+      'msg' => 'Taller registrado correctamente'
+    ]);
+  }
 }
