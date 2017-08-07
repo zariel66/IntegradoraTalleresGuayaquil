f9398f84d97b7a8ffe18175f9330fad551b03d8f
Karen Ponce <gatita89_pretty@hotmail.com>
2017-08-07 08:35:58 -0500

    [ADD] new services in ApiService (utils)

--- integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/utils/ApiService.java
+++ integradoramovil/TallerGuayaquil/app/src/main/java/com/example/karen/tallerguayaquil/utils/ApiService.java
@@ -4,6 +4,7 @@ import com.example.karen.tallerguayaquil.models.Api;
 import com.example.karen.tallerguayaquil.models.Brand;
 import com.example.karen.tallerguayaquil.models.Person;
 import com.example.karen.tallerguayaquil.models.Service;
+import com.example.karen.tallerguayaquil.models.Vehicle;
 import com.example.karen.tallerguayaquil.models.WorkShop;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
@@ -22,6 +24,14 @@ public interface ApiService {
 
     @GET("marcas/")
     Call<List<Brand>> getBrands();
+
+    @POST("vehiculos/")
+    Call<Api<List<Vehicle>>> getVehicles(@Body Person person);
+
+    @FormUrlEncoded
+    @POST("busquedataller/")
+    Call<Api<List<WorkShop>>> searchWorkshops(@FieldMap Map<String, String> params);
+
 
     /** SignUp**/
     @POST("registrotallersubmit/")
@@ -35,5 +45,6 @@ public interface ApiService {
     @FormUrlEncoded
     @POST("iniciarsesion/")
     Call<Api<Person>> login(@FieldMap Map<String, String> params);
+
 }
 
